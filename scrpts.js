bd0f8370ee79d28332017182ec59e3b1edac0979
snowflake125 <cfg21213@gmail.com>
2024-11-29 17:13:18 +1000

    в

--- scrpts.js
+++
@@ -1,9 +0,0 @@
-function toggleSection(element) {
-    const ul = element.nextElementSibling;
-    if (ul.style.display === "block") {
-      ul.style.display = "none";
-    } else {
-      ul.style.display = "block";
-    }
-  }
-  
